942f2631413a83f5961788ff008977ffe5b366aa
v-sinmo <v-sinmo@DESKTOP-7URMOJL.fareast.corp.microsoft.com>
2020-01-29 23:48:39 +0530

    page object

--- SeleniumCookbook/src/test/java/seleniumcookbook/tests/pageobjects/BmiCalcPage.java
+++ SeleniumCookbook/src/test/java/seleniumcookbook/tests/pageobjects/BmiCalcPage.java
@@ -3,48 +3,48 @@ package seleniumcookbook.tests.pageobjects;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.LoadableComponent;
+import static org.junit.Assert.*;
+public class BmiCalcPage extends LoadableComponent<BmiCalcPage> {
 
-public class BmiCalcPage {
-	private WebElement heightCMS;
-	private WebElement weightKg;
-	private WebElement Calculate;
-	private WebElement bmi;
-	private WebElement bmi_category;
-	private WebDriver driver;
-	private String url = "http://dl.dropbox.com/u/55228056/bmicalculator.html";
+	  private WebElement heightCMS;
+	  private WebElement weightKg;
+	  private WebElement calculate;
+	  private WebElement bmi;
+	  private WebElement bmiCategory;
 
-	public BmiCalcPage(WebDriver driver) {
-		this.driver = driver;
-		PageFactory.initElements(driver, this);
+	  private WebDriver driver;
+
+	  private String url = "http://cookbook.seleniumacademy.com/bmicalculator.html";
+	  private String title = "BMI Calculator";
+
+	  public BmiCalcPage(WebDriver driver) {
+	    this.driver = driver;
+	    PageFactory.initElements(driver, this);
+	  }
+
+	  @Override
+	  protected void load() {
+	    this.driver.get(url);
+	  }
+
+	  @Override
+	  protected void isLoaded() throws Error {
+	    assertTrue("Bmi Calculator page not loaded",
+	        driver.getTitle().equals(title));
+	  }
+
+	  public void calculateBmi(String height, String weight) {
+	    heightCMS.sendKeys(height);
+	    weightKg.sendKeys(weight);
+	    calculate.click();
+	  }
+
+	  public String getBmi() {
+	    return bmi.getAttribute("value");
+	  }
+
+	  public String getBmiCategory() {
+	    return bmiCategory.getAttribute("value");
+	  }
 	}
-
-	public void load() {
-		this.driver.get(url);
-	}
-
-	public void close() {
-		this.driver.close();
-	}
-
-	public void setHeight(String height) {
-		heightCMS.sendKeys(height);
-	}
-
-	public void setWeight(String weight) {
-		weightKg.sendKeys(weight);
-	}
-
-	public void calculateBmi(String height, String weight) {
-		heightCMS.sendKeys(height);
-		weightKg.sendKeys(weight);
-		Calculate.click();
-	}
-
-	public String getBmi() {
-		return bmi.getAttribute("value");
-	}
-
-	public String getBmiCategory() {
-		return bmi_category.getAttribute("value");
-	}
-}
--- SeleniumCookbook/src/test/java/seleniumcookbook/tests/pageobjects/BmiCalculatorTests.java
+++ SeleniumCookbook/src/test/java/seleniumcookbook/tests/pageobjects/BmiCalculatorTests.java
@@ -6,37 +6,34 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BmiCalculatorTests {
-	private WebDriver driver;
 
-	@Before
-	public void setUp() {
+	  private WebDriver driver;
+
+	  @Before
+	  public void setUp() {
 		System.setProperty("webdriver.chrome.driver", "./src/test/resources/drivers/chromedriver.exe");
-		driver = new ChromeDriver();
-		driver.get("http://cookbook.seleniumacademy.com/bmicalculator.html");
+	    driver = new ChromeDriver();
+	  }
+
+	  @Test
+	  public void testBmiCalculation() {
+	    // Create an instance of Bmi Calculator Page class
+	    // and provide the driver
+	    BmiCalcPage bmiCalcPage = new BmiCalcPage(driver);
+
+	    // Open the Bmi Calculator Page
+	    bmiCalcPage.get();
+
+	    // Calculate the Bmi by supplying Height and Weight values
+	    bmiCalcPage.calculateBmi("181", "80");
+
+	    // Verify Bmi & Bmi Category values
+	    assertEquals("24.4", bmiCalcPage.getBmi());
+	    assertEquals("Normal", bmiCalcPage.getBmiCategory());
+	  }
+
+	  @After
+	  public void tearDown() {
+	    driver.quit();
+	  }
 	}
-
-	@Test
-	public void testBmiCalculation() {
-		// Create an instance of Bmi Calculator Page class
-		// and provide the driver
-		BmiCalcPage bmiCalcPage = new BmiCalcPage(driver);
-
-		// Open the Bmi Calculator Page
-		bmiCalcPage.load();
-
-		// Calculate the Bmi by supplying Height and Weight values
-		bmiCalcPage.calculateBmi("181", "80");
-
-		// Verify Bmi & Bmi Category values
-		assertEquals("24.4", bmiCalcPage.getBmi());
-		assertEquals("Normal", bmiCalcPage.getBmiCategory());
-
-		// Close the Bmi Calculator Page
-		bmiCalcPage.close();
-	}
-
-	@After
-	public void tearDown() {
-		driver.quit();
-	}
-}
